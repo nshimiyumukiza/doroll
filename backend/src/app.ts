a138fe473b2dc27c8ec7ce4e3e4136f60e54c611
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-09-12 17:50:08 +0300

    istall cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./router/user.router.js";
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 app.use("/users",userRouter)
 
 dotenv.config()
@@ -12,7 +14,7 @@ dotenv.config()
 const port = process.env.PORT || 3000;
 const database = process.env.DATABASE;
 
-app.listen(port, ()=>{
+app.listen(port, () =>{
     console.log(`port is running on ${port}`)
 })
 
